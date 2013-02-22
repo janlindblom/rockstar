4b7326182d0a62223a4f2359521e39e78cc7997d
Jan Lindblom <jan@nyfagel.se>
2013-02-22 18:01:34 +0100

    There, starting to work.

--- app.js
+++ app.js
@@ -6,6 +6,9 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
+  , band = require('./routes/band')
+  , email = require('./routes/email')
+  , submission = require('./routes/submission')
   , http = require('http')
   , path = require('path');
 
@@ -14,15 +17,14 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'hjs');
+  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
+  app.use(express.cookieParser('my secret here'));
   app.use(express.session());
   app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
@@ -31,7 +33,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/topthree/:band', band.topthree);
+app.post('/submit', submission.validate);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
